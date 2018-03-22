7eedaf4791f1b4e424c597575c66ced380164936
praburaj.pandian <praburaj.pandian@CDC2-L-54650LS>
2018-03-22 12:24:12 +0530

    New request form check in

--- ProjectOnePlace/Asset/Scripts/release-module.js
+++ ProjectOnePlace/Asset/Scripts/release-module.js
@@ -1,4 +1,12 @@
 ﻿
-ngApp.controller('releaserequest', function ($scope, $http) {
+
+//NEW RELEASE REQUEST
+ngApp.controller('releasenewrequest', function ($scope, $http) {
+    $scope.name = 'Project one place'
+});
+
+
+//GET LIST OF RELEASE REQUEST
+ngApp.controller('releaserequestlist', function ($scope, $http) {
     $scope.name = 'Project one place'
 });
